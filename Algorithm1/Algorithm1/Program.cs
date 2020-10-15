62ea1f4eaa90c83c2d8e091b15813213707e5dad
philippine2 <49458817+philippine2@users.noreply.github.com>
2020-10-14 22:54:03 -0700

    Add files via upload

---
+++ Algorithm1/Algorithm1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4,2,1};
+            printOdd();
+            printArrayValue(arr);
+        }
+
+        // Printing Odd numbers 1 to 20
+        public static void printOdd()
+        {
+           
+           
+            for (int i = 1; i <20; i++)
+            {
+                if (i%2 == 1)
+                {
+                    Console.WriteLine(i +" is an odd num ");
+                }
+            }
+            Console.ReadLine();
+        }
+        
+
+        // Printin each array value and the sum so far
+        public static void printArrayValue(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+                Console.WriteLine( "num" + arr[i]+",Sum"+ sum);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+   
+}
